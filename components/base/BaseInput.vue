5ac4459f22f3b456949fe054e783afa3e6b00804
RSTLSS <72990855+rst-lss@users.noreply.github.com>
2022-05-30 22:06:24 +0430

    feat: base input style

--- components/base/BaseInput.vue
+++ components/base/BaseInput.vue
@@ -25,3 +25,40 @@ export default {
 }
 </script>
 
+<style scoped lang="scss">
+.base-input {
+  position: relative;
+  direction: rtl;
+
+  &::v-deep .v-input__slot {
+    border: 1px solid #d5d5d5;
+  }
+
+  &::v-deep .v-text-field__details {
+    position: absolute;
+    margin: 2px 0 0;
+    left: 100%;
+    z-index: 999;
+
+    .v-messages__message {
+      background-color: #f44336;
+      color: #fff;
+      padding: 10px;
+      border-radius: 6px;
+      text-align: center;
+      white-space: nowrap;
+      position: relative;
+
+      &::after {
+        content: '';
+        position: absolute;
+        top: 40%;
+        right: 100%;
+        border: 5px;
+        border-color: transparent #f44336 transparent transparent;
+        border-style: solid;
+      }
+    }
+  }
+}
+</style>
