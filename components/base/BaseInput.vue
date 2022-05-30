4fffcba9999aedd6f6f50e1ca051eb7f0b265823
RSTLSS <72990855+rst-lss@users.noreply.github.com>
2022-05-30 22:06:01 +0430

    feat: base input template
    a wrapper component that implements different style.

---
+++ components/base/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-text-field
+    class="base-input"
+    :value="value"
+    v-bind="$attrs"
+    placeholder="نام"
+    persistent-placeholder
+    solo
+    flat
+    dense
+    @input="$emit('input', $event)"
+  >
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+}
+</script>
+
