3528bda58aed4ab3297825e70605f4d454d11921
RSTLSS <72990855+rst-lss@users.noreply.github.com>
2022-06-01 13:35:59 +0430

    feat: phone form template
    a form to get users phone number and check if they have registered or not.

---
+++ components/PhoneForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-form
+    ref="phoneForm"
+    v-model="isValid"
+    lazy-validation
+  >
+    <!-- mobile input -->
+    <base-input
+      v-model="phone"
+      class="mb-2"
+      placeholder="مثال:‌ 09121234567"
+      :error-messages="errorMessage"
+      :rules="rules"
+      type="tel"
+      required
+      @focus="errorMessage = null"
+    ></base-input>
+
+    <!-- submit button -->
+    <v-btn
+      type="submit"
+      :disabled="!isValid"
+      :loading="loading"
+      color="#4C9F87"
+      large
+      block
+    >
+      ادامه
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+import BaseInput from '@/components/base/BaseInput.vue'
+export default {
+  name: 'PhoneForm',
+  components: {
+    BaseInput,
+  },
+  data() {
+    return {
+      phone: '',
+      isValid: true,
+      loading: false,
+      errorMessage: null,
+      rules: [
+        (input) => Boolean(input) || 'این فیلد الزامیست',
+        (input) => input.length === 11 || 'شماره تلفن باید 11 رقم باشد',
+      ],
+    }
+  },
+  methods: {
+}
+</script>
