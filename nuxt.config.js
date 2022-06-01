c53646a1d28ad8fb98f5dde0d217d29c1c963bc9
RSTLSS <72990855+rst-lss@users.noreply.github.com>
2022-06-01 09:24:38 +0430

    fix: baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://api.vandar.io/v2/',
   },
 
   // Auth module configuration: https://auth.nuxtjs.org/schemes/local
