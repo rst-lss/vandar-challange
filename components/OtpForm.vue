87dcc3f5f2956cf2194ceeda4d0b4a55c87e36dd
RSTLSS <amin.haeri.programming@gmail.com>
2022-06-02 22:34:06 +0430

    feat: otp form template
    this component is for verifying user phone number

---
+++ components/OtpForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-form
+    ref="otpForm"
+    v-model="isValid"
+    lazy-validation
+    @submit.prevent="submitOtp"
+  >
+    <v-otp-input v-model="key" length="5" @finish="submitOtp"></v-otp-input>
+    <v-btn type="submit" :disabled="!isValid" color="#4C9F87" large block>
+      ورود
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'OtpForm',
+  props: {
+    phone: { type: String, required: true },
+  },
+  data() {
+    return {
+      key: '',
+      isValid: false,
+      rules: [(input) => Boolean(input) || 'این فیلد الزامیست'],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
