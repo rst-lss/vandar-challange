c0c69732a4521f46dc295370f7590afc1eda0d2c
RSTLSS <amin.haeri.programming@gmail.com>
2022-06-03 01:25:00 +0430

    feat: password recover form template

---
+++ components/PasswordRecoveryForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-form
+    ref="recoveryForm"
+    v-model="isValid"
+    lazy-validation
+    @submit.prevent="submitPhone"
+  >
+    <!-- mobile input -->
+    <base-input
+      v-model="phone"
+      class="mb-2"
+      placeholder="مثال:‌ 09121234567"
+      :rules="phoneRules"
+      type="tel"
+      required
+    ></base-input>
+
+    <!-- password input -->
+    <base-input
+      v-model="password"
+      class="mb-2"
+      placeholder="رمز عبور جدید"
+      :rules="passwordRules"
+      type="tel"
+      required
+    ></base-input>
+
+    <!-- submit button -->
+    <v-btn
+      type="submit"
+      :disabled="!isValid"
+      :loading="loading"
+      class="mb-2"
+      color="#4C9F87"
+      large
+      block
+    >
+      ادامه
+    </v-btn>
+
+    <div class="text-center pa-4">
+      <a href="/login">انصراف</a>
+    </div>
+  </v-form>
+</template>
+
+<script>
+import BaseInput from '@/components/base/BaseInput.vue'
+export default {
+  name: 'PasswordRecoveryForm',
+  components: {
+    BaseInput,
+  },
+  data() {
+    return {
+      phone: '',
+      password: '',
+      isValid: true,
+      loading: false,
+      phoneRules: [
+        (input) => Boolean(input) || 'این فیلد الزامیست',
+        (input) => input.length === 11 || 'شماره تلفن باید 11 رقم باشد',
+      ],
+      passwordRules: [(input) => Boolean(input) || 'این فیلد الزامیست'],
+    }
+  },
+}
+</script>
